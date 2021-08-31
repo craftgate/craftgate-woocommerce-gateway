fe416a7a5c360f17bdbff2df80a50fc9f65fee18
Onur Polattimur <onur.polattimur@gmail.com>
2021-08-31 11:05:20 +0300

    add installment fee to order

--- craftgate-payment-gateway.php
+++ craftgate-payment-gateway.php
@@ -181,6 +181,9 @@ function init_woocommerce_craftgate_gateway()
 
                 // Checks payment error.
                 if (!isset($checkout_form_result->paymentError) && $checkout_form_result->paymentStatus === 'SUCCESS') {
+                    if ($checkout_form_result->installment > 1) {
+                        $this->update_order_for_installment($order, $checkout_form_result);
+                    }
                     $order->payment_complete();
                     $orderMessage = 'Payment ID: ' . $checkout_form_result->id;
                     $order->add_order_note($orderMessage, 0, true);
@@ -231,6 +234,29 @@ function init_woocommerce_craftgate_gateway()
                 'result' => 'success',
                 'redirect' => $order->get_checkout_payment_url(true),
             );
+        }
+
+        /** Adds tax fee to order.
+         * @param $order object WooCommerce Order
+         * @param $checkout_form_result object Checkout Form Result
+         */
+        private function update_order_for_installment($order, $checkout_form_result)
+        {
+            $installment = $checkout_form_result->installment;
+            $order_amount = $this->format_price($order->get_total());
+            $installment_fee = $checkout_form_result->paidPrice - $order_amount;
+            $order_fee = new stdClass();
+            $order_fee->id = 'Installment Fee';
+            $order_fee->name = __('Installment Fee', $this->text_domain);
+            $order_fee->amount = $installment_fee;
+            $order_fee->taxable = false;
+            $order_fee->tax = 0;
+            $order_fee->tax_data = array();
+            $order_fee->tax_class = '';
+            $order->add_fee($order_fee);
+            $order->calculate_totals(true);
+            update_post_meta($order->id, 'craftgate_installment_number', esc_sql($installment));
+            update_post_meta($order->id, 'craftgate_installment_fee', $installment_fee);
         }
 
         /**
